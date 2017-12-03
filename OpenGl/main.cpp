dd069076783de73cc6f9acc4e3860242cd3ee30d
Dathan Hansell <Dathan Hansell@LAPTOP-8DFK9LS4>
2017-12-03 00:10:55 -0800

    Fixed lib problems

--- OpenGl/main.cpp
+++ OpenGl/main.cpp
@@ -7,13 +7,8 @@
 Quaternion Q1;
 int main(int argc, char** argv)
 {
-	Q1.Rotate(30, 60, 90);
-	Q1.Print("Q1 ");
-	Q1.Rotate(250, 124, 135);
-	Q1.Print("Q1 ");
 
-
-	SDL_Delay(30000);
+	SDL_Delay(1000);
 	glutInit(&argc, argv);
 	
 	SDL_Quit();
